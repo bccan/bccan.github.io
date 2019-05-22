ab850c1bc018b1bac7452f1bca212c8b9e37398e
Ammar <ammarsuleman@gmail.com>
2019-05-21 22:03:51 -0400

    tt

--- test/olg-first/sw.js
+++ test/olg-first/sw.js
@@ -1,15 +1,15 @@
-importScripts('/cache-polyfill.js');
+// importScripts('/cache-polyfill.js');
 
 
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/index.html',
-       'Images/bg-1.png',
-       'Fonts/AkzidGroCFFXBdCn.otf'
-     ]);
-   })
- );
-});
+// self.addEventListener('install', function(e) {
+//  e.waitUntil(
+//    caches.open('airhorner').then(function(cache) {
+//      return cache.addAll([
+//        '/',
+//        '/index.html',
+//        'Images/bg-1.png',
+//        'Fonts/AkzidGroCFFXBdCn.otf'
+//      ]);
+//    })
+//  );
+// });
